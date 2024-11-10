f624b20a06fc9dac8f668bc1a27031d6e874b8d1
Harvir R <harvir.rajasanci@gmail.com>
2024-11-10 15:00:00 -0800

    Update Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,11 @@
-import { react } from 'react';
+import React from 'react';
 
 function Header() {
-    return (
-        <div>
-            <img src='./assets/logo.png' alt='munchmate logo'></img>
-        </div>
-    )
+  return (
+    <div className="w-full flex justify-center py-4 absolute top-20">
+      <img src="./assets/logo.png" alt="munchmate logo" className="h-auto max-h-16" />
+    </div>
+  );
 }
 
 export default Header;
