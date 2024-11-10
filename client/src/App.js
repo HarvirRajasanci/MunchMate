eda28f4cbd5e24c9c833d351a976948bbd67e43b
Harvir R <harvir.rajasanci@gmail.com>
2024-11-10 11:31:19 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p class="text-lg text-blue-700 font-semibold">
+  This is a simple paragraph styled with Tailwind CSS.
+</p>
+
     </div>
   );
 }
 
 export default App;
+
