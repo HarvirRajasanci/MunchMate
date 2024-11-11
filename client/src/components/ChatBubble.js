69b508bced7ec6d38e58cd2d823c3a471a8bcd16
Harvir R <harvir.rajasanci@gmail.com>
2024-11-10 16:14:29 -0800

    Update ChatBubble component

--- client/src/components/ChatBubble.js
+++ client/src/components/ChatBubble.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function ChatBubble() {
+function ChatBubble({heading, text}) {
   return (
     <div className="absolute top-20 translate-y-10 bg-white text-black p-4 rounded-xl shadow-lg max-w-xs m-10">
-        <h1 style={{ color: '#F36359' }}>Hello!</h1>
-        <p className="text-sm">Let's set up your preferences</p>
+        <h1 style={{ color: '#F36359' }}>{heading}</h1>
+        <p className="text-sm">{text}</p>
     </div>
 
   );
